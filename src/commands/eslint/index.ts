8a79baa2723241690dad9e6373d0d9193301fc36
alxjrvs@gmail.com <alxjrvs@gmail.com>
2022-04-12 21:47:25 -0400

    Update Eslint

--- src/commands/eslint/index.ts
+++ src/commands/eslint/index.ts
@@ -1,93 +1,122 @@
-import { execSync } from 'child_process'
-import * as fs from 'fs'
-import { merge } from 'lodash'
-
+import { writeFileSync } from 'node:fs'
+import { PackageInstaller } from '../../utils/package-installer'
 import PackageJson from '../../utils/package-json'
-import PackageInstaller from '../../utils/package-installer'
+import { Prettier } from '../prettier'
 
 const CONFIG_FILE_NAME = '.eslintrc.json'
 
 const CONFIG = `{
-  "extends": ["airbnb", "prettier"],
-  "parser": "babel-eslint",
-  "globals": {
-    "document": true,
-    "window": true,
-    "expect": true,
-    "beforeEach": true,
-    "describe": true,
-    "test": true,
-    "jest": true
+  "env": {
+      "browser": true,
+      "es2021": true
+  },
+  "plugins": [
+      "@typescript-eslint",
+      "regexp",
+      "prettier"
+  ],
+  "extends": [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:regexp/recommended",
+      "airbnb",
+      "airbnb-typescript",
+
+      "prettier"
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+      "project": "./tsconfig.json",
+      "createDefaultProgram": true
   },
   "rules": {
-    "arrow-body-style": [1, "as-needed"],
-    "class-methods-use-this": "off",
-    "func-names": ["error", "never"],
-    "import/no-webpack-loader-syntax": "off",
-    "import/order": [
-      "error",
-      {
-        "groups": [
-          "builtin",
-          "external",
-          "internal",
-          "sibling",
-          "index",
-          "parent"
-        ]
-      }
-    ],
-    "no-else-return": "off",
-    "no-underscore-dangle": "off",
-    "no-unused-vars": [
-      "error",
-      {
-        "vars": "local",
-        "varsIgnorePattern": "_",
-        "args": "after-used",
-        "argsIgnorePattern": "_"
-      }
-    ],
-    "no-use-before-define": "off",
-    "react/jsx-filename-extension": "off"
+      "arrow-body-style": [
+          1,
+          "as-needed"
+      ],
+      "class-methods-use-this": "off",
+      "func-names": [
+          "error",
+          "never"
+      ],
+      "import/no-webpack-loader-syntax": "off",
+      "import/order": [
+          "error",
+          {
+              "groups": [
+                  "builtin",
+                  "external",
+                  "internal",
+                  "sibling",
+                  "index",
+                  "parent"
+              ]
+          }
+      ],
+      "no-else-return": "off",
+      "no-underscore-dangle": "off",
+      "no-unused-vars": [
+          "error",
+          {
+              "vars": "local",
+              "varsIgnorePattern": "_",
+              "args": "after-used",
+              "argsIgnorePattern": "_"
+          }
+      ],
+      "no-use-before-define": "off",
+      "react/jsx-filename-extension": "off"
   }
 }`
 
-class Eslint {
-  public run() {
-    process.stdout.write('Setting up eslint...\n\n')
+export class Eslint {
+  static command = 'eslint'
 
-    this.installDependencies()
-      .then(this.writeConfig)
-      .then(this.updatePackageJson)
+  static async run() {
+    Prettier.run()
+
+    process.stdout.write(`Setting up ${Eslint.command}...\n\n`)
+
+    await Eslint.installDependencies()
+    await Eslint.writeConfig()
+    Eslint.updatePackageJson()
   }
 
-  private installDependencies() {
+  static async installDependencies() {
     return PackageInstaller.addDev(
-      'babel-eslint',
-      'eslint',
+      '@typescript-eslint/eslint-plugin',
+      '@typescript-eslint/parser',
+      Prettier.command,
+      Eslint.command,
       'eslint-config-airbnb',
+      'eslint-plugin-react-hooks',
+      'eslint-config-prettier',
+      'eslint-config-airbnb-typescript',
       'eslint-plugin-import',
       'eslint-plugin-jsx-a11y',
+      'eslint-plugin-prettier',
       'eslint-plugin-react',
-      'eslint-config-prettier',
+      'eslint-plugin-regexp',
+      'lint-staged',
+      'husky',
     )
   }
 
-  private writeConfig() {
-    process.stdout.write(
-      `Writing the following config to ${CONFIG_FILE_NAME}\n\n`,
-    )
+  static async writeConfig() {
+    process.stdout.write(`Writing the following config to ${CONFIG_FILE_NAME}\n\n`)
 
-    fs.writeFileSync(CONFIG_FILE_NAME, CONFIG)
+    writeFileSync(CONFIG_FILE_NAME, CONFIG)
 
     return Promise.resolve()
   }
 
-  private updatePackageJson() {
-    const scriptConfig: any = {
+  static updatePackageJson() {
+    const scriptConfig = {
       scripts: {
-        lint: "npm run eslint '**/*.js'",
+        lint: 'eslint . -c .eslintrc.json',
+      },
+      'lint-staged': {
+        '*.{js,jsx,ts,tsx}': 'eslint . -c .eslintrc.json --cache --fix',
       },
     }
 
@@ -96,5 +125,3 @@ class Eslint {
     packageJson.write()
   }
 }
-
-export default Eslint
