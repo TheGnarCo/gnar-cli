29876789775cb3b6a714b687d0e4614478168f43
alxjrvs@gmail.com <alxjrvs@gmail.com>
2022-04-12 21:47:07 -0400

    Update Package-json

--- src/utils/package-json/index.ts
+++ src/utils/package-json/index.ts
@@ -1,16 +1,16 @@
-import * as fs from 'fs'
+import { writeFileSync, readFileSync } from 'node:fs'
 import { merge } from 'lodash'
 
 class PackageJson {
   public contents: string
 
   constructor() {
-    const packageJson = fs.readFileSync('package.json').toString()
+    const packageJson = readFileSync('package.json').toString()
     this.contents = JSON.parse(packageJson)
   }
 
-  public merge(config: any): PackageJson {
-    const prettyConfig = JSON.stringify(config, null, 2)
+  public merge(config: Record<string, unknown>): PackageJson {
+    const prettyConfig = JSON.stringify(config, undefined, 2)
 
     process.stdout.write('Merging the following into your package.json... \n')
     process.stdout.write(`${prettyConfig}\n\n`)
@@ -21,8 +21,8 @@ class PackageJson {
   }
 
   public write() {
-    const formattedJson = JSON.stringify(this.contents, null, 2)
-    fs.writeFileSync('package.json', formattedJson)
+    const formattedJson = JSON.stringify(this.contents, undefined, 2)
+    writeFileSync('package.json', formattedJson)
   }
 }
 
