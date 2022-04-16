70358bcfdfda00e67aeae4cad718aedd86513775
alxjrvs@gmail.com <alxjrvs@gmail.com>
2022-04-16 16:45:54 -0400

    Foritfy tests

--- gnar.test.ts
+++ gnar.test.ts
@@ -18,16 +18,16 @@ describe('Gnar-CLI', () => {
     execCommand(`SKIP_VERSION=true yarn build --outDir .test-support/dist`)
   })
   describe.each([
-    ['eslint', '', JS_SETUP, `yarn lint ${ESLINT_TEST_HACKS}`],
-    ['prettier', '', JS_SETUP, 'yarn prettify'],
-    ['rails', `new ${RAILS_APP}`, 'gem install rails', `cd ${RAILS_APP} && bin/rspec`],
-  ])('%s', (command, commandArgs, setupCommands, testCommand) => {
+    ['eslint', JS_SETUP, `yarn lint ${ESLINT_TEST_HACKS}`],
+    ['prettier', JS_SETUP, 'yarn prettify'],
+    [`rails new ${RAILS_APP}`, 'gem install rails', `cd ${RAILS_APP} && bin/rspec`],
+  ])('%s', (command, setupCommands, testCommand) => {
     const path: string = commandTestPath(command)
 
     beforeAll(() => {
       execCommand(`mkdir -p ${path}`)
       execCommand(`cd ${path} && ${setupCommands}`)
-      execCommand(`cd ${path} && node ./../dist/index.js ${command} ${commandArgs}`)
+      execCommand(`cd ${path} && node ./../dist/index.js ${command}`)
     })
 
     it(`can successfully run ${testCommand}`, () => {
