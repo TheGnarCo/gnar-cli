22625febce0c0d16335896193c25f6d57a529f5c
alxjrvs@gmail.com <alxjrvs@gmail.com>
2022-04-16 16:43:38 -0400

    Foritfy tests

--- gnar.test.ts
+++ gnar.test.ts
@@ -4,7 +4,7 @@ import { execCommand } from './src/utils/exec-command'
 const JS_SETUP = [
   'yarn init --yes',
   'touch example.js',
-  'mkdir src',
+  'mkdir -p src',
   'touch src/example.js',
   'npx tsc --init',
   'yarn install',
@@ -13,6 +13,9 @@ const JS_SETUP = [
 const ESLINT_TEST_HACKS = '--no-eslintrc -c .eslintrc.json'
 
 describe('Gnar-CLI', () => {
+  beforeAll(() => {
+    execCommand(`SKIP_VERSION=true yarn build --outDir .test-support/dist`)
+  })
   describe.each([
     ['eslint', JS_SETUP, `yarn lint ${ESLINT_TEST_HACKS}`],
     ['prettier', JS_SETUP, 'yarn prettify'],
@@ -20,8 +23,7 @@ describe('Gnar-CLI', () => {
     const path: string = commandTestPath(command)
 
     beforeAll(() => {
-      execCommand(`mkdir -p .test-support/${command}`)
-      execCommand(`SKIP_VERSION=true yarn build --outDir .test-support/dist`)
+      execCommand(`mkdir -p ${path}`)
       execCommand(`cd ${path} && ${setupCommands}`)
       execCommand(`cd ${path} && node ./../dist/index.js ${command}`)
     })
--- tests/index.ts
+++ tests/index.ts
@@ -1,12 +1,9 @@
 import { readFileSync } from 'node:fs'
 
-export const commandTestPath = (command: string): string => `.test-support/${command}`
+export const commandTestPath = (command: string): string =>
+  `.test-support/${command.toLowerCase().split(' ').join('_')}`
 
-const readFile = (directory: string, filename: string): string =>
-  readFileSync(`${directory}/${filename}`, 'utf8')
-
-const bigStrip = (contents: string): string =>
-  contents.replace(/\r\n|\n|\r/g, '').replace(/\s/g, '')
-
-export const readTestFile = (command: string, filename: string): string =>
-  bigStrip(readFile(commandTestPath(command), filename))
+export const readTestFile = (path: string, filename: string): string =>
+  readFileSync(`${path}/${filename}`, 'utf8')
+    .replace(/\r\n|\n|\r/g, '')
+    .replace(/\s/g, '')
