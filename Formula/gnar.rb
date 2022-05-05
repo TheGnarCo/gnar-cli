1301e9d6024c1af1a88570391133fb524a9488e5
alxjrvs@gmail.com <alxjrvs@gmail.com>
2022-05-04 23:14:17 -0400

    Update Brew Forumla

--- Formula/gnar.rb
+++ Formula/gnar.rb
@@ -1,1 +1,14 @@
-Foo
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+class Gnar < Formula
+  desc 'Decorate your Apps with Gnarly opinions'
+  homepage 'https://github.com/TheGnarCo/gnar-cli'
+  url $URL
+  sha256 $SHA
+  license 'MIT'
+  version $VERSION
+
+  def install
+    bin.install 'gnar'
+  end
+end
