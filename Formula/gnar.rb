f4914c539c384c01ed5dc46dcb03829b7c42e667
Alex Jarvis <alxjrvs@users.noreply.github.com>
2022-05-06 13:07:44 -0400

    Automate Publish + Release (#43)
    * Basic work. Need to figure out script to bundle and build.
    
    * node14
    
    * pkg script
    
    * stepping towards automation. enabling dry-run to see....what happens!
    
    * Testing version bump
    
    * adding test release step
    
    * bump version back down, test action script as one action
    
    * fix script name
    
    * more universal (?) sed (?)
    
    * add tests to step for extra security
    
    * Testing Version Bump again
    
    * Remove tests, will be covered anyway
    
    * Fix upload url step
    
    * Tweaking to use new action
    
    * no double if
    
    * Better SHA
    
    * Better Better Sha
    
    * return to test
    
    * Finer grain bin/pkg
    
    * No cacheing. Hmm.
    
    * Setting input token
    
    * tweaks
    
    * fix DIR
    
    * Remove tag-and-release step
    
    * Bring version back down
    
    * Name + only run on pushes to main
    
    * Remove test apparatus
    
    * add Publishing, tweak scripts and Readme
    
    * properly inset list
    
    * Proper Readme Formatting
    
    * Re-enabling testing for cross-repo push testing
    
    * add PR script, slim down publish, add dummy formula
    
    * slim down even more
    
    * remove strings
    
    * Antoher attempt at interpolation, also more slim
    
    * getting closer
    
    * Change BKP
    
    * rehydrate, do it the long way
    
    * Slightly more hydrated test, checking with version
    
    * Update Brew Forumla
    
    * Slim down, test up
    
    * test version bump
    
    * Better Bump
    
    * better output, curious what happens
    
    * testing better outputs?
    
    * use single quotes
    
    * Use Better Quotes
    
    * can I get the keys>
    
    * Even Better quotes?
    
    * escape?
    
    * URL
    
    * Return double quotes
    
    * try assets again
    
    * ooooh, fromJson
    
    * Maybe To Json?
    
    * direct approach
    
    * use real (fake) names
    
    * fix name
    
    * maybe quotes?
    
    * Just gonna hipfire this one.
    
    * Back to basics
    
    * Change names to better match jobs
    
    * Trying more tacticts.
    
    * Lowercase?
    
    * toJSon for good measure
    
    * try pretty-printing the echo
    
    * does pulling from `HEAD` work, I wonder
    
    * Oh damn, that worked
    
    * from, not to, json
    
    * fix escape
    
    * What if: No escape?
    
    * no escape for any
    
    * trying a ddifferent delimiter
    
    * Turn on to only do pushes to main
    
    * return test step
    
    * use real tag name
    
    * Go back to normal version
    
    * turn off dry-run
    
    * Add to readme
    
    * Remove Bkp
    
    * nest `if` steps
    
    * add auto-generated comment
    
    * Simpler PKG, no shasum
    
    * Swap build and test
    
    * Append Body
    
    * Putting into test mode
    
    * Version bump
    
    * Revert "Putting into test mode"
    
    This reverts commit f1b677339697658552016faaad0fe81cf084d9a6.
    
    * Revert "Version bump"
    
    This reverts commit 27f0973ed415477503e2d767b630271d0e3fe6e5.

---
+++ Formula/gnar.rb
@@ -0,0 +1,16 @@
+# Generated from Github Workflow
+# See https://github.com/TheGnarCo/gnar-cli for more information
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+class Gnar < Formula
+  desc 'Decorate your Apps with Gnarly opinions'
+  homepage 'https://github.com/TheGnarCo/gnar-cli'
+  url '$URL'
+  sha256 '$SHA'
+  license 'MIT'
+  version '$VERSION'
+
+  def install
+    bin.install 'gnar'
+  end
+end
